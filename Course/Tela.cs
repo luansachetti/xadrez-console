91a9a33e72c7dcec7d05017bf4e3ec93881a69e7
Luan Sachetti <luan.sachetti@hotmail.com>
2024-04-25 19:52:15 -0300

    Implementando a verificacao de xeque

--- Course/Tela.cs
+++ Course/Tela.cs
@@ -13,6 +13,10 @@ namespace Course
             Console.WriteLine();
             Console.WriteLine("Turno: " + partida.turno);
             Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+            if (partida.xeque)
+            {
+                Console.WriteLine("XEQUE!");
+            }
         }
 
         public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
