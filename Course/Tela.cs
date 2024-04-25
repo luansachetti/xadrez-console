7e5a69b5f95918bac0731785070cf67a5cc29909
Luan Sachetti <luan.sachetti@hotmail.com>
2024-04-25 16:19:30 -0300

    Fazendo os primeiros testes de movimentacao

--- Course/Tela.cs
+++ Course/Tela.cs
@@ -1,4 +1,5 @@
 ﻿using tabuleiro;
+using xadrez;
 namespace Course
 {
     internal class Tela
@@ -23,6 +24,14 @@ namespace Course
                 Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
         }
 
         public static void imprimirPeca(Peca peca)
